fff16dd02cd8528bef624af8fc301e5921ab0fc4
pablodherrero <pablodherrero@fa294226-2c99-11de-a28d-194e1977add9>
2009-04-27 09:59:20 +0000

    lara lara lara hey!, lara lara lara

--- src/asm/recortar.asm
+++ src/asm/recortar.asm
@@ -13,7 +13,7 @@
 
 %define ancho_sprite_bytes [ebp-4]
 %define basura_instancia [ebp-8]
-%define ancho_instancia_bytes [ebp-12]
+%define defasaje [ebp-12]
 %define final [ebp-16]
 
 global recortar
@@ -24,16 +24,18 @@ entrada_funcion 12
 
 	mov esi, ptrSprite
 	mov edi, ptrResultado
-	calcular_pixels ebx,ancho_instancia
-	mov ancho_instancia_bytes, ebx					;ancho de la instancia sobre el sprite en pixeles (sin la basura)
-	calcular_basura eax, ebx
-  mov basura_instancia,eax								;basura para la instancia
-  
+	calcular_pixels ebx,ancho_instancia			;ebx: ancho de la instancia sobre el sprite en pixeles (sin la basura)
+	calcular_basura eax, ebx								;basura para la instancia
+  mov basura_instancia,eax								
+	  
 	mov eax, instancia
-	mul ebx                                 ;tengo la cant de bytes hasta la primera instancia
+	mul ebx                                 ;tengo en edx:eax la cant de bytes hasta la primera instancia
 	add esi, eax                            ;en esi tengo el comienzo de la instancia dentro del sprite
 	
-	calcular_pixels ecx, ancho_sprite				;cantidada de pixeles q ocupa el sprite
+	sub ebx, 03h														;cantidad de bytes para avanzar del primer al ultimo pixel de una fila
+	mov defasaje, ebx												
+	
+	calcular_pixels ecx, ancho_sprite				;cantidad de pixeles q ocupa el sprite
   calcular_basura ebx,ecx  								;basura del sprite
   add ecx, ebx
   mov ancho_sprite_bytes, ecx							;ancho del sprite en pixeles (ecx)
@@ -43,43 +45,30 @@ entrada_funcion 12
   add eax,esi															;a esto le sumo el principio de la instancia para obtener donde termina esta
   mov final, eax             							;salvo el final de la instancia
 
+	mov eax, -03h														;guardar en eax, el sentido en que se mueve esi
+	cmp dword orientacion, 0
+	je seguir
+	mov dword defasaje, 0										;si se mueve hacia la derecha:
+  neg eax 
 seguir:  
   mov ecx, ancho_instancia								;ecx funciona de contador, indica por q pixel de la fila se encuenta el bucle
-	cmp dword orientacion, 0
-  jne ciclo									 
-  add esi, ancho_instancia_bytes 					;si hay q espejar, ir hasta el ultimo elemento de la fila
-  sub esi, 03h
-ciclo:              ;voy copiando los bytes de cada fila de la instancia y dejandolos en *res
-	mov bl, [esi] 		;edi es el puntero al byte actual de la instancia
-	mov [edi], bl
+	mov edx, esi														;guardar en edx, la pos al principio de la iteracion
+  add esi, defasaje							 					;si hay q espejar, ir hasta el ultimo elemento de la fila
+ciclo:              											
+	mov bl, [esi] 													;voy copiando los bytes de cada fila de la instancia y dejandolos en *res
+	mov [edi], bl														;edi es el puntero al byte actual de la instancia
 	
 	mov bx, [esi + 1] 		
 	mov [edi + 1], bx
 
-	add edi, 03h	
-	
-	cmp dword orientacion,0
-	je restar	
-	add esi, 03h
-	jmp fin_ciclo
-restar:
-	sub esi, 03h
-
-fin_ciclo:
-	loopne ciclo
-	
+	add edi, 03h														;anvanzo un pixel en el buffer destino
+	add esi, eax														;anvanzo o retrocedo un pixel en el sprite origen
+	loopne ciclo														;cuando el contador se hace 0 salir del bucle
+finalizacion:	
 	add edi, basura_instancia
-	
-	cmp dword orientacion,0
-	je no_restar
-	sub esi, ancho_instancia_bytes
-	jmp seguir2
-no_restar:
-	add esi, 3h
-seguir2:
-	
-	add esi, ancho_sprite_bytes										;paso a la siguiente fila
-	cmp esi, final																;si se llego al final del sprite, terminar
+	mov esi, edx														;recupero el valor al principio del iteracion
+	add esi, ancho_sprite_bytes							;y sumo para pasar a la siguiente fila
+	cmp esi, final													;si se llego al final del sprite, terminar
 	jne seguir
 	
 salida_funcion 12
