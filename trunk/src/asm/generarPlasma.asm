87bfd8dfeba891cc9f3fab6a8bdf4d46aa32edf7
pablodherrero <pablodherrero@fa294226-2c99-11de-a28d-194e1977add9>
2009-04-28 11:32:52 +0000

    cuasi-optimizaciones

--- trunk/src/asm/generarPlasma.asm
+++ trunk/src/asm/generarPlasma.asm
@@ -11,11 +11,9 @@ extern g_hor1
 ; el numero %2. %1 puede ser: 0 (R), 1 (G) o 2 (B) (debe ser inmediato). %2
 ; puede ser un registro o un inmediato de 8 bits. color_de_fondo utiliza
 ; internamente eax y edx para realizar calculos.
-%macro color_de_fondo 2
+%macro color_de_fondo_old 2
 
-    mov eax, SCREEN_W*3
-    ; tener en cuenta que esto toca eax y edx
-    mul i               ; eax = i*SCREEN_W*3
+    load_screenw_pixels
 
     ; uso edx porque total ya lo arruine con el mul...
     lea edx, [j + j * 2] ; edx = j + j*2 = j*3
@@ -23,20 +21,40 @@ extern g_hor1
 
     mov eax, [screen_pixeles]
     
-    mov byte [eax + edx + %1], %2
+    mov byte [eax + edx +%1], %2
 
 %endmacro
 
+%macro color_de_fondo 3
+    mov byte [%1 + %2], %3
+%endmacro
 
+%macro load_screenw_pixels 0
+    cmp dword res_mult, mult_invalid
+    je %%calcular
+    mov eax, res_mult
+    jmp %%mult_salida
+%%calcular:
+    mov eax, SCREEN_W*3
+    ; tener en cuenta que esto toca edx
+    mul i                               ; eax = j*SCREEN_W*3
+    mov res_mult, eax                   ; cacheo el resultado de mult
+%%mult_salida:
+%endmacro
+
+%define mult_invalid 0xFFFFFFFF
+    
 global generarPlasma
 
 %define i esi
 %define j edi
 
 %define rgb [ebp + 8]
+%define res_mult [ebp - 4]
 generarPlasma:
-    entrada_funcion 0
+    entrada_funcion 4
 
+    mov dword res_mult, mult_invalid    ;resetear valor de mult
     xor i, i
 loop_i:
 
@@ -86,9 +104,7 @@ loop_j:
     add ecx, 128                        ; ecx es index
     and ecx, 0xFF
 
-    mov eax, SCREEN_W*3
-    ; tener en cuenta que esto toca edx
-    mul i                               ; eax = j*SCREEN_W*3
+    load_screenw_pixels
 
     mov ebx, [screen_pixeles]
     lea edx, [ j + j * 2 ]
@@ -112,6 +128,11 @@ ir_a_seguir:
     ; aca viene el switch
 entrar_al_switch:
 
+    load_screenw_pixels ;cargo en eax, el desplazamiento vertical (en bytes)
+    lea edx, [j + j * 2] ; edx = j + j*2 = j*3
+    add eax, edx        ; eax = j*3 + i*SCREEN_W*3
+    add eax, [screen_pixeles]   ;queda todo el offset completo en eax
+
 case_1:
     cmp cl, 64
     jae case_2                          ; fallaba por q jge es signed
@@ -121,9 +142,9 @@ case_1:
     sub bl, cl
     dec bl                              ; bl = 255 - ((index << 2) + 1)
 
-    color_de_fondo 0,bl
-    color_de_fondo 1,cl
-    color_de_fondo 2,0
+    color_de_fondo eax,0,bl
+    color_de_fondo eax,1,cl
+    color_de_fondo eax,2,0
     
     jmp seguir
 
@@ -134,9 +155,9 @@ case_2:
     shl cl, 2
     inc cl                              ; cl = (index << 2) + 1
 
-    color_de_fondo 0,cl
-    color_de_fondo 1,255
-    color_de_fondo 2,0
+    color_de_fondo eax,0,cl
+    color_de_fondo eax,1,255
+    color_de_fondo eax,2,0
 
     jmp seguir
 
@@ -149,9 +170,9 @@ case_3:
     sub bl, cl
     dec bl                             ; bl = 255 - ((index << 2) + 1)
 
-    color_de_fondo 0,bl
-    color_de_fondo 1,bl
-    color_de_fondo 2,0
+    color_de_fondo eax,0,bl
+    color_de_fondo eax,1,bl
+    color_de_fondo eax,2,0
 
     jmp seguir
 
@@ -162,24 +183,26 @@ case_4:
     shl cl, 2
     inc cl                             ; cl = (index << 2) + 1
 
-    color_de_fondo 0,cl
-    color_de_fondo 1,0
-    color_de_fondo 2,0
+    color_de_fondo eax,0,cl
+    color_de_fondo eax,1,0
+    color_de_fondo eax,2,0
     
     jmp seguir
 
 case_5:
 
-    color_de_fondo 0,0
-    color_de_fondo 1,0
-    color_de_fondo 2,0
+    color_de_fondo eax,0,0
+    color_de_fondo eax,1,0
+    color_de_fondo eax,2,0
 
 
 seguir:
-
+    
     inc j
     cmp j, SCREEN_W
     jl loop_j
+    
+    mov dword res_mult, mult_invalid    ;resetear valor de mult    
 
     inc i
     cmp i, SCREEN_H
@@ -188,4 +211,4 @@ seguir:
     add word [g_ver0], 9
     add word [g_hor0], 8
 
-    salida_funcion 0
+    salida_funcion 4
