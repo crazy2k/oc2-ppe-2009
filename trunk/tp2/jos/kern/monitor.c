3d6e68854e74e46c6d9c38343fec80de7876d81e
pablodherrero <pablodherrero@fa294226-2c99-11de-a28d-194e1977add9>
2009-06-14 07:07:41 +0000

    arreglado monitor

--- trunk/tp2/jos/kern/monitor.c
+++ trunk/tp2/jos/kern/monitor.c
@@ -70,8 +70,8 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 	/* bootstacktop es el simbolo q respresenta la posicion de la base del stack, 
 	 * por lo q una lectura de su valor solo nos da los primeros 4 bytes del stack
 	*/
-    while ((void*) ebp < (void*) bootstacktop) {
-
+	cprintf("\t ebp: %x\t stacktop: %x\n", ebp, bootstacktop);
+    while(1) {
         uint32_t eip = *(ebp + 1);
 
         cprintf("\t ebp %x\t eip %x\t args ", ebp, eip);
@@ -81,8 +81,10 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
             cprintf("%x ", *(ebp + 1));
 
         cprintf("\n");
+		
+		if ((void*) ebp + 8 >= ((void*) bootstacktop)) break;
 
-        ebp += 1 + 1 + 5 ; // ebp + eip + params
+        ebp = (uint32_t *) *ebp;
 
     }
 
